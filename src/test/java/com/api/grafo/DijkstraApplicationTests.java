b71d29608f292681c1c3bac7dcacc0418184599a
joaovitorpagani99 <joaovitorpagani7@hotmail.com>
2024-02-08 12:54:39 -0300

    atualizando nomeclatura

--- src/test/java/com/api/grafo/DijkstraApplicationTests.java
+++ src/test/java/com/api/grafo/DijkstraApplicationTests.java
@@ -1,13 +1,35 @@
 package com.api.grafo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DijkstraApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testDistanciaRotaABC() throws Exception {
+		mockMvc.perform(post("/distance")
+				.contentType("application/json")
+				.content("{\"path\": [\"A\", \"B\", \"C\"]}"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("9"));
+	}
+
+	@Test
+	void testRecuperarGrafo() throws Exception {
+		mockMvc.perform(get("/graph/{graphId}", 1L))
+				.andExpect(status().isOk());
 	}
 
 }
